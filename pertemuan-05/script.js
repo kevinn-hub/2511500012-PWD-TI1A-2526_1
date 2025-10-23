a7013d5470a404bb515d21a224e74cd14c278773
kevinn-hub <2511500012@mahasiswa.atmaluhur.ac.id>
2025-10-23 16:39:32 +0700

    a

--- pertemuan-05/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("menuToggle").addEventListener("clik", function () {
-    document.querySelector("nav").classList.toggle("active");
-});
-alert("Halo Dunia dari file eksternal!");
-let nama = prompt("Siapa nama kamu?");
-alert("Halo, " + nama + "!");
