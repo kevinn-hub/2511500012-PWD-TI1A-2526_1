9656ed858bdb6f6575c556fd730e2b9727dc529a
kevinn-hub <2511500012@mahasiswa.atmaluhur.ac.id>
2025-11-04 16:19:43 +0700

    index.php

--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Judul Halaman</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style.css?=v2">
 </head>
 <body>
 
