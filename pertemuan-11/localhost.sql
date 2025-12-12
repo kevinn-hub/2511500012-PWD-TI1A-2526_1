d9644f7adaac02d64eed029253d59d209a608d1a
kevinn-hub <2511500012@mahasiswa.atmaluhur.ac.id>
2025-12-12 09:07:38 +0700

    file myqsl

--- pertemuan-11/localhost.sql
+++ pertemuan-11/localhost.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 12, 2025 at 12:58 AM
+-- Generation Time: Dec 12, 2025 at 02:06 AM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
@@ -42,10 +42,13 @@ CREATE TABLE `tbl_tamu` (
 --
 
 INSERT INTO `tbl_tamu` (`cid`, `cnama`, `cemail`, `cpesan`, `dcreated_at`) VALUES
-(1, 'Leonard Kevin Fernando', '2511500012@mahasiswa.atmaluhur.ac.id', 'ayo yang teliti belajar pemrograman dasarnya,jangan kebanyakan typo', '2025-12-12 07:49:50'),
-(2, 'Alexsande Morgan Des Passito', 'Alexsander Morgan @gmail.com', 'Syalom', '2025-12-12 07:49:50'),
-(3, 'Marsen Sitepu', 'Marsen@gmail.com', 'Selamat sore semuanya', '2025-12-12 07:49:50'),
-(5, 'sinta', 'sinta@gmail.com', 'sudah selesai belum tugasnya', '2025-12-12 07:58:17');
+(1, 'kevin', 'aku@gmail.com', 'selamat malam semuanya', '2025-12-12 09:02:19'),
+(2, 'sinta', 'sinta@gmail.com', 'halo nama aku sinta', '2025-12-12 09:02:43'),
+(3, 'bagus', 'sumatera@gmail.com', 'apa kabar kita semua yang ada di sini apakah sehat', '2025-12-12 09:03:22'),
+(4, 'burtihan', 'musang@gmail.com', 'halo email aku musang', '2025-12-12 09:03:47'),
+(5, 'jodi', 'aku@gmail.com', 'hujan banjir semua nya ada di sini apakah kalian terjebak hujan yang tidak berhenti ini', '2025-12-12 09:04:39'),
+(6, 'agus', 'sumatera@gmail.com', 'halo nama aku agus', '2025-12-12 09:05:01'),
+(7, 'setian', 'sumatera@gmail.com', 'selamat malam dan selamat natal untuk kita semua semoga impian kita terwujud', '2025-12-12 09:06:05');
 
 --
 -- Indexes for dumped tables
@@ -65,7 +68,7 @@ ALTER TABLE `tbl_tamu`
 -- AUTO_INCREMENT for table `tbl_tamu`
 --
 ALTER TABLE `tbl_tamu`
-  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
